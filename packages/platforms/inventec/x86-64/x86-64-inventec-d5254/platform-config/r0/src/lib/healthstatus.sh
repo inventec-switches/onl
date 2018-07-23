540744c14885d6c4dfb72ffa9ea0c781a49f0e0f
alang <huang.alang@inventec.com>
2018-07-23 10:57:54 +0800

    modify health led behaviour by following Gulmohar_HW_Specification_V0.8_20180702

--- packages/platforms/inventec/x86-64/x86-64-inventec-d5254/platform-config/r0/src/lib/healthstatus.sh
+++ packages/platforms/inventec/x86-64/x86-64-inventec-d5254/platform-config/r0/src/lib/healthstatus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#follow Gulmohar_HW_Specification_V0.8_20180702
 normal='0 : normal'
 unpowered='2 : unpowered'
 fault='4 : fault'
@@ -8,6 +8,8 @@ FAN_UNPLUG_NUM=0
 FAN_LED_RED='fan_led_red'
 NUM=1
 FAN_NUM=5
+FIRST_READ=0
+SECOND_READ=0
 #PSU_STAUS='000'
 #switch is ready , transfer control of cpld to cpu
 echo 1 > /sys/bus/i2c/devices/i2c-0/0-0055/ctl
@@ -15,30 +17,56 @@ echo 1 > /sys/bus/i2c/devices/i2c-0/0-0055/ctl
 while true
 do
 
-NUM=1
-FAN_UNPLUG_NUM=0
 #monitor how many fan modules are unplugged
-while [  $NUM -le $FAN_NUM ]; do
-        if [ $(cat /sys/class/hwmon/hwmon1/$FAN_LED_RED$NUM) -eq 1 ]
-        then
-          let FAN_UNPLUG_NUM=FAN_UNPLUG_NUM+1       
-        fi
-        let NUM=NUM+1
-done
 
-#echo "FAN_UNPLUG_NUM:$FAN_UNPLUG_NUM"
+
+#first check
+FAN_UNPLUG_NUM=0
+FAN_ARR=$(cat /sys/class/hwmon/hwmon1/$FAN_LED_RED?)
+
+while read -r line; do
+    fan_led_red_check=$(echo "$line")
+    if [ $fan_led_red_check -eq 1 ]
+    then
+    let FAN_UNPLUG_NUM=FAN_UNPLUG_NUM+1
+    fi 
+done <<< "$FAN_ARR"
+FIRST_READ=$FAN_UNPLUG_NUM
+
+#second check
+FAN_UNPLUG_NUM=0
+FAN_ARR=$(cat /sys/class/hwmon/hwmon1/$FAN_LED_RED?)
+
+while read -r line; do
+    fan_led_red_check=$(echo "$line")
+    if [ $fan_led_red_check -eq 1 ]
+    then
+    let FAN_UNPLUG_NUM=FAN_UNPLUG_NUM+1
+    fi 
+done <<< "$FAN_ARR"
+SECOND_READ=$FAN_UNPLUG_NUM
+
+if [ $FIRST_READ -ne $SECOND_READ ]
+then
+  #echo "not equl:$FIRST_READ != $SECOND_READ"
+  continue
+fi 
 
 if [ $FAN_UNPLUG_NUM -ge 2 ]
 then
   #echo "solid red"  
   echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
   echo 0 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
+  sleep 1
+  continue
 elif [ $FAN_UNPLUG_NUM -eq 1 ]
 then
-  #echo "blink at 1hz in red"
-  echo 2 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
-  echo 0 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
-else
+  #solid orange
+  echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
+  echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
+  sleep 1
+  continue
+fi
 
   #echo "normal"
   psu0var=$(cat /sys/bus/i2c/devices/i2c-0/0-0055/psu0)    # bottom PSU
@@ -50,20 +78,18 @@ else
       #solid green
       echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
       echo 0 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
+      #echo "solid green"
    else  
       if [ "$psu0var" = "$unpowered" ] ||
       [ "$psu1var" = "$unpowered" ]
       then
-        #echo "grn blink at hz"
-        echo 2 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
-        echo 0 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
-      else
-        #solid red
+        #echo solid orange
+        echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
         echo 7 > /sys/bus/i2c/devices/i2c-0/0-0055/red_led
-        echo 0 > /sys/bus/i2c/devices/i2c-0/0-0055/grn_led
       fi
-   fi #not both normal
-fi
+      
+   fi 
+
 
 sleep 1
 done
